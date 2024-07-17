f487f87a563f919911798aa6bdbef2f5f9e0fe56
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2024-07-18 00:30:49 +0530

    project final commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  console.log("Socket On");
+  socket.on("send-location", (data) => {
+    io.emit("recieve-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+const PORT = 8040;
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
---
+++ public/js/script.js
@@ -0,0 +1,46 @@
+const socket = io();
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+      console.log(latitude, "--", longitude);
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+
+const map = L.map("map").setView([0, 0], 20);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "Aman Vishwakarma Maps",
+}).addTo(map);
+
+const marker = {};
+
+socket.on("recieve-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 20);
+
+  if (marker[id]) {
+    marker[id].setLatLng;
+    [latitude, longitude];
+  } else {
+    marker[id] = L.marker([latitude, longitude], 20).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (marker[id]) {
+    map.removeLayer(marker.id);
+    delete marker[id];
+  }
+});
