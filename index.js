3110a509bd73bdf1bcd8b5ecf4cb5b46b7820238
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2024-07-20 00:37:40 +0530

    index.js update for vercel.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 io.on("connection", (socket) => {
   console.log("Socket On");
